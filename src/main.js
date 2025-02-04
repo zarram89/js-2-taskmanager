37265f7c83c80fe5d2ab8e407c282ad8e9e38d5d
Ramil Zaripov <zarram89@gmail.com>
2025-02-05 00:12:46 +0500

    Adds board presenter

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import NewTaskButtonView from './view/new-task-button-view';
 import FilterView from './view/filter-view.js';
+import BoardPresenter from './presenter/board-presenter.js';
 import { render } from './render';
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
+const boardPresenter = new BoardPresenter({boardContainer: siteMainElement});
 
 render(new NewTaskButtonView(), siteHeaderElement);
 render(new FilterView(), siteMainElement);
+
+boardPresenter.init();
