23890ff14f74fb908329a57fcffe863b39ca4815
Ramil Zaripov <zarram89@gmail.com>
2025-02-04 23:39:35 +0500

    Adds button add new task

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-console.log('Hello');
+import NewTaskButtonView from './view/new-task-button-view';
+import { render } from './render';
+
+const siteMainElement = document.querySelector('.main');
+const siteHeaderElement = siteMainElement.querySelector('.main__control');
+
+render(new NewTaskButtonView(), siteHeaderElement);
