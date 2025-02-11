6ef033373abfd03740992659d700a59aaa6b9482
Ramil <zarram89@gmail.com>
2025-02-11 18:29:09 +0500

    4.1.4 Добавит классам приватные поля и геттеры

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,32 +1,31 @@
-import {render} from '../framework/render.js';
-import BoardView from '../view/board-view.js';
-import SortView from '../view/sort-view.js';
-import TaskListView from '../view/task-list-view.js';
-import TaskView from '../view/task-view.js';
-import TaskEditView from '../view/task-edit-view.js';
 import LoadMoreButtonView from '../view/load-more-button-view.js';
 
 export default class BoardPresenter {
-  boardComponent = new BoardView();
-  taskListComponent = new TaskListView();
+  #boardContainer = null;
+  #tasksModel = null;
 
-  constructor({ boardContainer, taskModel }) {
-    this.boardContainer = boardContainer;
-    this.taskModel = taskModel;
+  #boardComponent = new BoardView();
+  #taskListComponent = new TaskListView();
+
+  #boardTasks = [];
+
+  constructor({boardContainer, tasksModel}) {
+    this.#boardContainer = boardContainer;
+    this.#tasksModel = tasksModel;
   }
 
   init() {
-    this.boardTasks = [...this.taskModel.getTasks()];
+    this.#boardTasks = [...this.#tasksModel.tasks];
 
-    render(this.boardComponent, this.boardContainer);
-    render(new SortView(), this.boardComponent.element);
-    render(this.taskListComponent, this.boardComponent.element);
-    render(new TaskEditView({task: this.boardTasks[0]}), this.taskListComponent.element);
+    render(this.#boardComponent, this.#boardContainer);
+    render(new SortView(), this.#boardComponent.element);
+    render(this.#taskListComponent, this.#boardComponent.element);
+    render(new TaskEditView({task: this.#boardTasks[0]}), this.#taskListComponent.element);
 
-    for (let i = 1; i < this.boardTasks.length; i++) {
-      render(new TaskView({task: this.boardTasks[i]}), this.taskListComponent.element);
+    for (let i = 1; i < this.#boardTasks.length; i++) {
+      render(new TaskView({task: this.#boardTasks[i]}), this.#taskListComponent.element);
     }
 
-    render(new LoadMoreButtonView(), this.boardComponent.element);
+    render(new LoadMoreButtonView(), this.#boardComponent.element);
   }
 }
