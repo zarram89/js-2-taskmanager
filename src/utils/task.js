a9c4d42fdd22b6764bfe7cef49159684ec0144b7
Ramil <zarram89@gmail.com>
2025-03-11 09:06:17 +0500

    4.1.10 Отрефакторит модуль со вспомогательными функциями

--- src/utils/task.js
+++ src/utils/task.js
@@ -2,10 +2,6 @@ import dayjs from 'dayjs';
 
 const DATE_FORMAT = 'D MMMM';
 
-function getRandomArrayElement(items) {
-  return items[Math.floor(Math.random() * items.length)];
-}
-
 function humanizeTaskDueDate(dueDate) {
   return dueDate ? dayjs(dueDate).format(DATE_FORMAT) : '';
 }
@@ -18,5 +14,5 @@ function isTaskRepeating(repeating) {
   return Object.values(repeating).some(Boolean);
 }
 
-export {getRandomArrayElement, humanizeTaskDueDate, isTaskExpired, isTaskRepeating};
+export { humanizeTaskDueDate, isTaskExpired, isTaskRepeating };
 
